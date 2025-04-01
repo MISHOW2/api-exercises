a79a55c683ef4d9c721cfc6e17584594e837d17e
MISHOW2 <mshibambo63@gmail.com>
2025-04-02 01:17:28 +0200

    group by category

--- exercises/4-getUniqueCategories.js
+++ exercises/4-getUniqueCategories.js
@@ -1,19 +1,15 @@
-const url = 'http://localhost:5000/api/products'
+const url = 'http://localhost:5000/api/products';
 
-//  Fetch only product catergory
-const getUniqueCategories = async ()=>{
+// Fetch unique product categories
+const getUniqueCategories = async () => {
   const response = await fetch(url);
   const data = await response.json();
-  
-  const categories = data.filter(product=>{
-    if (product.category === 'shoe') {
-      return product.name
-    }
-  })
 
-  console.log(categories);
-  
-  return categories
-}
+  // Extract only category names
+  const categories = [...new Set(data.map(product => product.category))];
 
-getUniqueCategories()
+  console.log(categories); // Output unique categories
+  return categories;
+};
+
+getUniqueCategories();
